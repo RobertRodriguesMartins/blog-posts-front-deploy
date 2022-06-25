49c35ef5cec28708b760e0689051e6cdb3a80fdb
Robert Rodrigues <robert.rm131@gmail.com>
2022-06-25 10:34:14 -0300

    ADD: nav component

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav>
+      <h2>title</h2>
+      <ul>
+        <li>Criar Noticia</li>
+        <li>Minhas Noticias</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar;
