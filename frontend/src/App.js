957e6c0101431bf6fe532552e3c10fc3c007260c
Robert Rodrigues <robert.rm131@gmail.com>
2022-06-26 10:41:00 -0300

    FEAT: new section for news
    - moved all content about news to his component "News"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import './App.css';
-import NavBar from './components/NavBar.js';
+import NavBar from './components/';
+import { News } from './components/';
 
 function App() {
-  const news = useSelector((state) => state.news);
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
       </header>
+      <section className='app-news-section'>
+        <News />
+      </section>
     </div>
   );
 }
