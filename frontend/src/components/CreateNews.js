3b28953136fd22ef04cd6fad007d215994282881
Robert Rodrigues <robert.rm131@gmail.com>
2022-06-26 17:14:34 -0300

    FEAT: a component to create news
    - [WIP]

---
+++ frontend/src/components/CreateNews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CreateNews() {
+  return (
+    <section className='app-news-create-wrapper'>
+      <h1>Create news</h1>
+    </section>
+  );
+}
+
+export default CreateNews;
