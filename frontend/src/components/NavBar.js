d546d1aee9e130190f24b3caf8b4ad0c8fe76680
Robert Rodrigues <robert.rm131@gmail.com>
2022-06-26 10:31:51 -0300

    FEAT: new icon and section "search"
    - now you have an input to search for specific news. not implemented yet

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { BsSearch } from 'react-icons/bs';
 
 function NavBar() {
   return (
-    <nav>
-      <h2>title</h2>
-      <ul>
-        <li>Criar Noticia</li>
-        <li>Minhas Noticias</li>
-      </ul>
+    <nav className="nav-menu">
+      <h2>Portal Robert News</h2>
+      <section className="nav-tools">
+        <ul>
+          <li>Criar Noticia</li>
+          <li>Minhas Noticias</li>
+        </ul>
+        <section className="search">
+          <label htmlFor="search">
+            <BsSearch />
+          </label>
+          <input id='search' type='text' placeholder="search a notice" ></input>
+        </section>
+      </section>
     </nav>
   )
 }
