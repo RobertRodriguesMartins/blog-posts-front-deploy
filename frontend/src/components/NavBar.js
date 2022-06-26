c2f4bce6133463a083a1e2854a388fe3c6c996dc
Robert Rodrigues <robert.rm131@gmail.com>
2022-06-26 17:23:42 -0300

    FEAT: 'criar noticia' field is mapped to route
    - It's mapped to /news/create route, wich maps CreateNews component

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 import { BsSearch } from 'react-icons/bs';
 
 function NavBar() {
@@ -7,14 +8,14 @@ function NavBar() {
       <h2>Portal Robert News</h2>
       <section className="nav-tools">
         <ul>
-          <li>Criar Noticia</li>
+          <Link to="/news/create"><li>Criar Noticia</li></Link>
           <li>Minhas Noticias</li>
         </ul>
         <section className="search">
           <label htmlFor="search">
             <BsSearch />
           </label>
-          <input id='search' type='text' placeholder="search a notice" ></input>
+          <input id='search' type='text' placeholder="Buscar noticias..." ></input>
         </section>
       </section>
     </nav>
