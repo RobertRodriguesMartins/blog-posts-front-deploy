858fb559e5a158ee5d9bf45a526022e672b5e000
Robert Rodrigues <robert.rm131@gmail.com>
2022-06-26 16:51:02 -0300

    FEAT: findById and exists services.
    - exists service will only check if a given id exists, findByid will get it.

--- backend/src/service/News.ts
+++ backend/src/service/News.ts
@@ -1,4 +1,4 @@
-import NewsEntity,{ News } from '../interface/News';
+import NewsEntity, { News } from '../interface/News';
 import connection from '../model/db/connection';
 import NewsModel from '../model/News';
 
@@ -8,8 +8,17 @@ class NewsService implements NewsEntity<News> {
   public create = async (notice: News): Promise<News | void> => {
     return this.newsModel.create(notice);
   }
+
   public findAll = async (): Promise<News[]> => {
     return this.newsModel.findAll();
+  }
+
+  public exists = async (id: number): Promise<boolean> => {
+    return this.newsModel.exists(id);
+  }
+
+  public findById = async (id: number): Promise<News> => {
+    return this.newsModel.findById(id);
   }
 }
 
