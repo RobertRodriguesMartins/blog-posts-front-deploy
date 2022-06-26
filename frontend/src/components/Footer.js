82e4982a1f4b9df126616623ff8849fef09e5794
Robert Rodrigues <robert.rm131@gmail.com>
2022-06-26 17:13:28 -0300

    FEAT: Footer component

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+
+  return (
+    <section className="footer-section">
+      <p>Desenvolvido por <em>Robert Rodrigues Martins</em></p>
+    </section>
+  )
+}
+
+export default Footer;
