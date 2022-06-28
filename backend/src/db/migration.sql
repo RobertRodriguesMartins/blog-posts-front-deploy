f01172ff59ede7923de5b03a0a95e6e62f7c89d7
Robert Rodrigues <robert.rm131@gmail.com>
2022-06-27 22:35:32 -0300

    FIX: db content now is a text field

--- backend/src/db/migration.sql
+++ backend/src/db/migration.sql
@@ -12,7 +12,7 @@ create table `category`(
 create table `news`(
 	`id` int primary key auto_increment,
     `title` varchar(50) not null,
-    `content` varchar(300) not null,
+    `content` text not null,
     `category_name` varchar(50) not null,
     foreign key (`category_name`) references `category`(`name`)
 );
