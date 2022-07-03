f020bf9a049f30c2448141baf9e24af2d0cfab52
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-03 20:13:19 -0300

    FEAT: navBar changes, [WIP]

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,25 +1,47 @@
-import React from "react";
-import { Link } from 'react-router-dom'
-import { BsSearch } from 'react-icons/bs';
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
+import { HiDotsVertical } from "react-icons/hi";
 
 function NavBar() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    document
+      .getElementById(pathname)
+      .setAttribute("class", "current-nav-section");
+  }, [pathname]);
   return (
     <nav className="nav-menu">
-      <h2>Portal Robert News</h2>
+      <Link to="/">
+        <h2>Portal Robert News</h2>
+      </Link>
       <section className="nav-tools">
         <ul>
-          <Link to="/news/create"><li>Criar Noticia</li></Link>
+          <Link to="/">
+            <li id="/">Home</li>
+          </Link>
+          <Link to="/news/create">
+            <li id="/news/create">Criar Noticia</li>
+          </Link>
           <li>Minhas Noticias</li>
         </ul>
         <section className="search">
           <label htmlFor="search">
             <BsSearch />
           </label>
-          <input id='search' type='text' placeholder="Buscar noticias..." ></input>
+          <input
+            id="search"
+            type="text"
+            placeholder="Buscar noticias..."
+          ></input>
         </section>
       </section>
+      <section className="mobile-nav-menu">
+        <HiDotsVertical className="nav-icon" />
+      </section>
     </nav>
-  )
+  );
 }
 
 export default NavBar;
