45f9739c59cfd1ac8bf4970fc1ecfd00ba2610d4
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-03 20:12:08 -0300

    FEAT: CreateNews component

--- frontend/src/components/CreateNews.js
+++ frontend/src/components/CreateNews.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { createThunk } from '../redux/action/news';
-import NavBar from './NavBar';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createThunk } from "../redux/action/news";
+import NavBar from "./NavBar";
 
 function CreateNews() {
   const dispatch = useDispatch();
@@ -12,28 +12,28 @@ function CreateNews() {
   }
 
   return (
-    <div className='app-news-create-wrapper'>
+    <div className="app-news-create-wrapper">
       <header className="app-header">
         <NavBar />
       </header>
-      <section className='app-news-create-form-wrapper'>
-        {
-          form !== 'submitted' ? (
-            <section>
-              <h1>Edite sua Notícia</h1>
-              <form method='POST' onSubmit={submitForm}>
-                <label htmlFor='title'>Título</label>
-                <input id='title' type='text' name='title' required />
-                <label htmlFor='content'>Conteúdo</label>
-                <input id='content' type='textarea' name='content' maxLength={500} required />
-                <label htmlFor='category'>Categoria</label>
-                <input id='category' name='categoryName' type='text' required />
-                <button type='submit'>Postar</button>
-              </form>
-            </section>
-          ) : <div>Enviado com sucesso!</div>
-        }
-        <section className='app-news-create-lastnews '>
+      <section className="app-news-create-form-wrapper">
+        {form !== "submitted" ? (
+          <section>
+            <h1>Edite sua Notícia</h1>
+            <form method="POST" onSubmit={submitForm}>
+              <label htmlFor="title">Título</label>
+              <input id="title" type="text" name="title" required />
+              <label htmlFor="content">Conteúdo</label>
+              <textarea id="content" name="content" rows="20" required />
+              <label htmlFor="category">Categoria</label>
+              <input id="category" name="categoryName" type="text" required />
+              <button type="submit">Postar</button>
+            </form>
+          </section>
+        ) : (
+          <div>Enviado com sucesso!</div>
+        )}
+        <section className="app-news-create-lastnews ">
           <h2>Últimas notícias!</h2>
           <div>Em breve...</div>
         </section>
