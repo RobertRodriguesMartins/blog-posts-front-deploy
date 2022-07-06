822586695f1aa1ee54309d86f0b2e0792ce48df0
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-05 21:15:06 -0300

    FEAT: /some and /offset routes
    - /some get some news using offset
    - /offset returns the max offset possible for "news" table

--- backend/routes.ts
+++ backend/routes.ts
@@ -6,6 +6,8 @@ const routes = Router();
 const news = new NewsController();
 
 routes.post('/', news.create);
+routes.get('/some', news.some);
+routes.get('/offset', news.maxOffset);
 routes.get('/', news.findAll);
 routes.get('/:id', news.findById);
 
