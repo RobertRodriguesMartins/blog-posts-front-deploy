86ac82c158f96050f6dcdba6e1e2a06618d85792
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-05 21:27:26 -0300

    FEAT: reducer cases news/some, [offset/ add , max]

--- frontend/src/redux/reducer/news.js
+++ frontend/src/redux/reducer/news.js
@@ -2,30 +2,46 @@ const initialState = {
   allNews: [],
   createdNews: [],
   inProgressNew: {},
-  specificNews: {},
-  form: ''
-}
+  maxOffset: 0,
+  actualOffset: 0,
+  form: '',
+};
 
 const newsReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'news/all':
       return {
         ...state,
-        allNews: action.payload
-      }
+        allNews: action.payload,
+      };
+    case 'news/some':
+      return {
+        ...state,
+        someNews: action.payload,
+      };
     case 'news/byId':
       return {
         ...state,
-        specificNews: action.payload
-      }
+        specificNews: action.payload,
+      };
     case 'news/create':
       return {
         ...state,
-        form: action.payload ? 'error' : 'submitted'
-      }
+        form: action.payload ? 'error' : 'submitted',
+      };
+    case 'offset/add':
+      return {
+        ...state,
+        actualOffset: action.payload + 6,
+      };
+    case 'offset/max':
+      return {
+        ...state,
+        maxOffset: action.payload,
+      };
     default:
       return state;
   }
-}
+};
 
 export default newsReducer;
