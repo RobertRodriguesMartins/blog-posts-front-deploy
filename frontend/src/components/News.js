8c08f5bdc1fd8bfb323447ca0044208ded2d05f5
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-07 16:20:31 -0300

    FIX: duplicated react map key

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import NewsCard from './NewsCard';
-import Footer from './Footer';
 
 function News() {
   const { allNews } = useSelector((state) => state.news);
+
+  useEffect(() => {
+    console.log('WIP...');
+  }, [allNews]);
   return (
     <>
       <h2>Todas as noticias</h2>
@@ -13,14 +16,13 @@ function News() {
           <section className="app-news-card-article-wrapper">
             {allNews && allNews.length > 0 ? (
               allNews.map((notice) => {
-                return <NewsCard news={notice} key={notice.id} />;
+                return (
+                  <NewsCard news={notice} key={notice.id + Math.random()} />
+                );
               })
             ) : (
               <h2>Sem noticias no momento... :0</h2>
             )}
-            <footer className="footer-wrapper">
-              <Footer />
-            </footer>
           </section>
         </section>
       </section>
