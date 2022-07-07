cb9bc82746c9e2725e2c74ff25a87907487281c9
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-07 16:17:43 -0300

    FEAT: infinite scroll implementation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,59 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import NavBar from './components/';
-import { News } from './components/';
+import { News, Footer } from './components/';
 import { maxOffsetThunk, someThunk, setOffset } from './redux/action/news';
 
 function App() {
   const dispatch = useDispatch();
-  const actualOffset = useSelector((state) => state.news.actualOffset);
-  const maxOffset = useSelector((state) => state.news.maxOffset);
+  const [actualOffset, setActualOffset] = useState(0);
+  const [maxOffset, setMaxOffset] = useState(0);
+  const [loading, setLoading] = useState(0);
+  const actualReduxOffset = useSelector((state) => state.news.actualOffset);
+  const actualReduxMaxOffset = useSelector((state) => state.news.maxOffset);
+
+  function checkIfExists(e, actualOffset, maxOffset) {
+    const scrollSection = e.target;
+    const { scrollHeight, scrollTop, clientHeight } = scrollSection;
+    if (clientHeight + scrollTop >= scrollHeight) {
+      if (actualOffset <= maxOffset * 6) {
+        setLoading(true);
+      }
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    if (actualOffset <= maxOffset * 6 && loading) {
+      dispatch(someThunk(actualOffset));
+      dispatch(setOffset(actualOffset));
+      setLoading(false);
+    }
+  }, [loading]);
+
   useEffect(() => {
     const scrollSection =
       document.getElementsByClassName('app-news-section')[0];
-    scrollSection.addEventListener('scroll', () => {
-      const { scrollHeight, scrollTop, clientHeight } = scrollSection;
-      console.log(actualOffset, maxOffset);
-      if (clientHeight + scrollTop >= scrollHeight) {
-        if (actualOffset <= maxOffset * 6) {
-          dispatch(someThunk(actualOffset));
-          dispatch(setOffset(actualOffset));
-        }
-        return false;
-      }
-    });
-  }, [actualOffset]);
+    scrollSection.addEventListener('scroll', (e) =>
+      checkIfExists(e, actualOffset, maxOffset)
+    );
+
+    return scrollSection.removeEventListener('scroll', (e) =>
+      checkIfExists(e, actualOffset, maxOffset)
+    );
+  }, []);
 
   useEffect(() => {
     dispatch(someThunk(actualOffset));
     dispatch(maxOffsetThunk());
     dispatch(setOffset(actualOffset));
   }, []);
+
+  useEffect(() => {
+    setActualOffset(actualReduxOffset);
+    setMaxOffset(actualReduxMaxOffset);
+  }, [actualReduxMaxOffset, actualReduxOffset]);
+
   return (
     <div className="app">
       <header className="app-header">
@@ -37,6 +62,9 @@ function App() {
       <section className="app-news-section">
         <News />
       </section>
+      <footer className="footer-wrapper">
+        <Footer />
+      </footer>
     </div>
   );
 }
