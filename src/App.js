b94147cd55540bb21a14986572293bed78e3eeef
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-08 20:24:56 -0300

    FIX: infinite issue
    - it wasn't  fetching new posts when user comes from create post

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { API_URL } from './api';
 import NavBar from './components/';
 import { News, Footer } from './components/';
-import { maxOffsetThunk, someThunk, setOffset } from './redux/action/news';
+import {
+  maxOffsetThunk,
+  someThunk,
+  setOffset,
+  reset,
+  setLastPostsNumber,
+} from './redux/action/news';
 
 function App() {
   const dispatch = useDispatch();
   const [actualOffset, setActualOffset] = useState(0);
   const [maxOffset, setMaxOffset] = useState(0);
   const [loading, setLoading] = useState(0);
+  const lastPostsNumber = useSelector((state) => state.news.totalPosts);
   const actualReduxOffset = useSelector((state) => state.news.actualOffset);
   const allNews = useSelector((state) => state.news.allNews);
   const actualReduxMaxOffset = useSelector((state) => state.news.maxOffset);
+
+  async function getMaxPosts() {
+    const rawData = await fetch(API_URL, {
+      method: 'GET',
+    });
+    const { total } = await rawData.json();
+    return total;
+  }
 
   function checkIfExists(e, actualOffset, maxOffset) {
     const scrollSection = e.target;
@@ -46,11 +62,27 @@ function App() {
 
   useEffect(() => {
     if (allNews.length < 1) {
-      dispatch(someThunk(actualOffset));
-      dispatch(maxOffsetThunk());
-      dispatch(setOffset(actualOffset));
+      getMaxPosts().then((res) => {
+        dispatch(someThunk(actualOffset));
+        dispatch(maxOffsetThunk());
+        dispatch(setOffset(actualOffset));
+        dispatch(setLastPostsNumber(res));
+      });
     }
   }, []);
+
+  useEffect(() => {
+    if (allNews.length > 1) {
+      getMaxPosts().then((total) => {
+        if (total > lastPostsNumber && lastPostsNumber !== 0) {
+          dispatch(someThunk(0));
+          dispatch(reset());
+          dispatch(maxOffsetThunk());
+          dispatch(setOffset(0));
+        }
+      });
+    }
+  }, [lastPostsNumber]);
 
   useEffect(() => {
     setActualOffset(actualReduxOffset);
