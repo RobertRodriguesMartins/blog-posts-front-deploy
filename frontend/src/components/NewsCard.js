d00fa89e8b11c83d8ed3777ef1482fc878c4c6b9
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-07 21:14:04 -0300

    FEAT: gives more emphasis to post author

--- frontend/src/components/NewsCard.js
+++ frontend/src/components/NewsCard.js
@@ -1,23 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function NewsCard(props) {
-
   const notice = props.news;
   return (
     <article>
       <Link to={`/news/${notice.id}`}>
         <h1>{notice.title}</h1>
         <p>{notice.content}</p>
-        <span>{notice.categoryName}</span>
+        <span>
+          ~<em>{notice.categoryName}</em>
+        </span>
       </Link>
     </article>
-  )
+  );
 }
 
 NewsCard.propTypes = {
-  news: PropTypes.object
-}
+  news: PropTypes.object,
+};
 
 export default NewsCard;
