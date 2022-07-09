056b1a1b9924867cc8c45942da5bf5c85b2f806c
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-09 12:55:31 -0300

    FEAT<newsCard>: [WIP]

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function NewsCard(props) {
+  const tpCache = [0, 0];
+  function double_click(ev) {
+    const timestamp = new Date().getTime();
+    tpCache[0] = tpCache[1];
+    tpCache[1] = timestamp;
+    if (tpCache[1] - tpCache[0] < 300) {
+      console.log('double click');
+    }
+  }
+
   const notice = props.news;
+  useEffect(() => {
+    document
+      .getElementsByClassName('touch')[0]
+      .addEventListener('touchstart', double_click);
+  }, []);
   return (
-    <article>
+    <article className="touch">
       <Link to={`/news/${notice.id}`}>
         <h1>{notice.title}</h1>
         <p>{notice.content}</p>
