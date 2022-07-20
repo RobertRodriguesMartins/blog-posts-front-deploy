37dfb4da796b01cd6e33a959b13521ae4f31c292
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-20 16:28:44 -0300

    FIX<component>: no publications case

--- src/components/News.js
+++ src/components/News.js
@@ -6,17 +6,19 @@ function News() {
   const { allNews } = useSelector((state) => state.news);
 
   useEffect(() => {
-    console.log('WIP...');
+    console.log('Wip...');
   }, [allNews]);
   return (
     <section className="app-news-card-wrapper">
       <section className="app-news-card-article-wrapper">
-        {allNews && allNews.length > 0 ? (
+        {allNews && allNews[0] ? (
           allNews.map((post) => {
             return <NewsCard news={post} key={post.id + Math.random()} />;
           })
+        ) : allNews[0] === 0 ? (
+          <h2>{`Ainda sem publicações, seja o primeiro! <3`}</h2>
         ) : (
-          <h2>carregando os posts... :0</h2>
+          <h2 className="post-loading">Carregando posts... :0</h2>
         )}
       </section>
     </section>
