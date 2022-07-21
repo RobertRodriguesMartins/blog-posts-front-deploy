c77f59149973f83faed741290203ce50b29af582
Robert Rodrigues <robert.rm131@gmail.com>
2022-07-21 18:59:10 -0300

    UPDATE<component>: handler

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,8 @@ import News from "./News";
 import Footer from "./Footer";
 import NewsDetails from "./NewsDetails";
 import CreateNews from "./CreateNews";
+import CreateUser from "./CreateUser";
 
 export default NavBar;
 
-export { News, Footer, NewsDetails, CreateNews };
+export { News, Footer, NewsDetails, CreateNews, CreateUser };
